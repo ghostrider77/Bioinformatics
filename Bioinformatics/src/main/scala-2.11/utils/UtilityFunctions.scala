27613541f12f9177f44e2b325da78a0c1f3932f6
Tamas Kurics <tamas.kurics@gmail.com>
2017-07-23 06:08:37 +0200

    add utility function for IO operations
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Bioinformatics/src/main/scala-2.11/utils/UtilityFunctions.scala
@@ -0,0 +1,36 @@
+package utils
+
+import java.io.{BufferedWriter, File, FileWriter, InputStream}
+
+object UtilityFunctions {
+
+  def readInputData(filename: String): List[String] = {
+    val fileStream: InputStream = getClass.getResourceAsStream(filename)
+    val lines: Iterator[String] = scala.io.Source.fromInputStream(fileStream).getLines()
+    lines.toList
+  }
+
+  def convertStringToIntList(line: String, sep: String = " "): List[Int] = line.split(sep).map(_.toInt).toList
+
+  def writeListAsStringToFile[T](lst: List[T], outputFilename: String = "output.txt", sep: String = " "): Unit = {
+    val file = new File(outputFilename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    val result: String = lst.mkString(sep)
+    bw.write(result)
+    bw.close()
+  }
+
+  def writeListOfListsAsStringsToFile[T](lists: List[List[T]],
+                                         outputFilename: String = "output.txt",
+                                         sep: String = " "): Unit = {
+    val file = new File(outputFilename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    for { lst <- lists } {
+      val line: String = lst.mkString(sep)
+      bw.write(line)
+      bw.write("\n")
+    }
+    bw.close()
+  }
+
+}
